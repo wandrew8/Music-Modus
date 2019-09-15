82f57f1d3e124ba16b21b1bc55eb61361b0e09a7
Andrew Weiss <wandrew8@gmail.com>
2019-09-15 13:56:15 -0700

    Successfully pulling lyrics and appending to page

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,10 +1,17 @@
-var artist = $("#artistInput").val().trim();
-var artistName = [];
+//Variables
 
+var artist = $("#artistInput").val().trim();
+var songTitle = $("#songTitleInput").val().trim();
+
+var artistNameArray = [];
+var songTitleArray = [];
+
+
+//Function to display artist's name, photo, and biography
 function displayArtistInfo() {
 
     var artistN = $(this).attr("data-name");
-    var queryURL = "https://www.theaudiodb.com/api/v1/json/1/search.php?s=" + artistName;
+    var queryURL = "https://www.theaudiodb.com/api/v1/json/1/search.php?s=" + artistNameArray;
     
 
     $.ajax({
@@ -16,7 +23,8 @@ function displayArtistInfo() {
 
 
         $("#artistBio").text(response.artists[0].strBiographyEN);
-        $("#artistName").text(artistName[0].toUpperCase());
+        var capitalizedName = capital_letter(artistNameArray[0])
+        $("#artistName").text(capitalizedName);
     
         console.log(response.artists[0].strBiographyEN);
         console.log(response.artists[0].strArtistThumb);
@@ -26,35 +34,87 @@ function displayArtistInfo() {
     });   
 }
 
+//Function to capitalize the first letter of each word
+function capital_letter(str) 
+{
+    str = str.split(" ");
+
+    for (var i = 0, x = str.length; i < x; i++) {
+        str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+    }
+
+    return str.join(" ");
+}
+
+
+//Function to display the lyrics on the page
+function displayLyrics() {
+
+  var queryURL = "https://api.lyrics.ovh/v1/" + artistNameArray + "/" + songTitleArray + "/";
+  
+
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+  }).then(function (response) {
+
+      $("#lyricText").append(response.lyrics)
+
+
+      
+  });   
+}
+
+
+//On click command for the submit button
 $("#submitButton").on("click", function(event) {
     event.preventDefault();
 
     var artist = $("#artistInput").val().trim();
+    var songTitle = $("#songTitleInput").val().trim();
+
+    //Conditional to display modal prompting user to insert the needed information
+    if (artist === "" || songTitle === "") {
+      $(".bg-modal").css("display", "flex");
+
+    }
 
     $(".image-cropper").css("background-image", "url()");
     $("#artistBio").empty();
     $("#artistName").empty();
+    $("#lyricText").empty();
     $("#artistInput").val("");
     $("#songTitleInput").val("");
-    artistName = []
-    artistName.push(artist);
+    artistNameArray = []
+    artistNameArray.push(artist);
+    songTitleArray = [];
+    songTitleArray.push(songTitle);
+
     console.log("You selected the following artist: " + artist);
     displayArtistInfo();
+    displayLyrics();
     
 })
 
+//On click command to close the modal
+$(".closeButton").on("click", function(event) {
+  event.preventDefault();
+
+  $(".bg-modal").css("display", "none");
+
+});
 
 
 
 
-//Slide in left animation
+//Slide in left animation code
 var tl = new TimelineMax({onUpdate:updatePercentage});
 var tl2 = new TimelineMax();
 const controller = new ScrollMagic.Controller();
 
 tl.from('#artistPhotoContainer', 0.8, {x:-200, opacity: 0,ease: Power4.easeInOut}, "=-1");
-tl.from('#artistInfo', 0.5, {x:-200, opacity: 0,ease: Power4.easeInOut}, "=-1");
-tl.from('#songMeter', 0.2, {x:-200, opacity: 0,ease: Power4.easeInOut}, "=-1");
+tl.from('#artistInfo', 0.6, {x:-200, opacity: 0,ease: Power4.easeInOut}, "=-1");
+tl.from('#songMeter', 0.5, {x:-200, opacity: 0,ease: Power4.easeInOut}, "=-1");
 
 
 const scene = new ScrollMagic.Scene({
