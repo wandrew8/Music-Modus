6750807583a5e309bf0d0c1079e4aa729f1296eb
wheeters <wheetmansw@gmail.com>
2019-09-17 15:15:06 -0700

    working

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,13 +1,6 @@
 var lyricHold;
 
 function displayArtistInfo(songTitle, artist) {
-
-
-
- 
-
-    var artistN = $(this).attr("data-name");
-    var queryURL = "https://www.theaudiodb.com/api/v1/json/1/search.php?s=" + artistNameArray;
   var queryURL =
     "https://www.theaudiodb.com/api/v1/json/1/search.php?s=" + artist;
 
@@ -30,48 +23,13 @@ function displayArtistInfo(songTitle, artist) {
       method: "GET"
     }).then(function(responseLyrics) {
       queryURL =
-        "https://www.purgomalum.com/service/plain?text=" + artist + responseLyrics.lyrics;
+        "https://www.purgomalum.com/service/plain?text=" + artist
+        responseLyrics.lyrics
+        ;
 
       $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function (response) {
-
-        $(".image-cropper").css("background-image", "url(" + response.artists[0].strArtistThumb + ")");
-        $("#artistBio").text(response.artists[0].strBiographyEN);
-        var capitalizedName = capital_letter(artistNameArray[0])
-        $("#artistName").text(capitalizedName);
-    
-
-
-        queryURL = `https://api.lyrics.ovh/v1/${artist}/${songTitle}`;
-       
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function (responseLyrics) {
-       
-       
-        queryURL = "https://www.purgomalum.com/service/plain?text=" + responseLyrics.lyrics;
-
-
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function (response) {
-
-
-
-        var lyricLocation = $('#lyricText')
-        lyricLocation.empty()
-        lyricLocation.attr("dataCensored", response)
-        lyricLocation.attr("dataUncensored", responseLyrics.lyrics)
-        lyricLocation.attr("state", "uncensored")
-        lyricLocation.text(responseLyrics.lyrics)
-
-        lyricHold = responseLyrics.lyrics
-        
-
       }).then(function(response) {
         var lyricLocation = $("#lyricText");
         lyricLocation.empty();
@@ -109,31 +67,9 @@ function displayArtistInfo(songTitle, artist) {
           );
         });
       });
-      
-        })
-
-
-      })
-
-    })
-    console.log("this is search term in testing", artist + songTitle)
-    $.ajax({
-      url: "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + artist + songTitle + "&key=AIzaSyD-I609EBue7TuGbhT7_bn6PawQPIxN2wI&",
-      method: "GET"
-    }).then(function (response) {
-      console.log("response", response)
-      var videoId = response.items[0].id.videoId
-      console.log("videoId", videoId)
-      $("#videoOutPut").append(`<iframe width="79%" height="78%" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
-
-
-    })
-
-
-
     });
-  };
-
+  });
+}
 
 //Function to capitalize the first letter of each word
 function capital_letter(str) {
@@ -146,91 +82,7 @@ function capital_letter(str) {
   return str.join(" ");
 }
 
-
-
-//Function to display the lyrics on the page
-function displayLyrics() {
-
-    //Main content appears on the page
-    $("#mainContent").css("display", "block");
-    $("#moreContent").css("display", "block");
-  var queryURL = "https://api.lyrics.ovh/v1/" + artistNameArray + "/" + songTitleArray + "/";
-
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function (response) {
-
-    $("#lyricText").append(response.lyrics)
-
-    console.log("Here are the lyrics: " + response)
-
-    if (songTitleArray === response.error) {
-      $(".bg-modal").css("display", "flex");
-
-    }
-
-  });
-
-  for (var i = 0, x = str.length; i < x; i++) {
-    str[i] = str[i][0].toUpperCase() + str[i].substr(1);
-  }
-
-  return str.join(" ");
-}
-
 //On click command for the submit button
-$("#submitButton").on("click", function (event) {
-  event.preventDefault();
-
-  var artist = $("#artistInput").val().trim();
-  var songTitle = $("#songTitleInput").val().trim();
-
-  //Main content appears on the page
-  $("#mainContent").css("display", "flex");
-  $("#moreContent").css("display", "flex");
-
-  $([document.documentElement, document.body]).animate({
-    scrollTop: $("#mainContent").offset().top
-  }, 2000);
-
-  //Conditional to display modal prompting user to insert the needed information
-  if (artist === "" || songTitle === "") {
-    $(".bg-modal").css("display", "block");
-  }
-
-
-  $(".image-cropper").css("background-image", "url()");
-  $("#artistBio").empty();
-  $("#artistName").empty();
-  $("#lyricText").empty();
-  $("#artistInput").val("");
-  $("#songTitleInput").val("");
-  $("#videoOutPut").empty();
-  artistNameArray = []
-  artistNameArray.push(artist);
-  songTitleArray = [];
-  songTitleArray.push(songTitle);
-
-  console.log("You selected the following artist: " + artist);
-  displayArtistInfo(songTitle, artist);
-
-  $("#profanityFilter").on("click", function () {
-    console.log("clicked")
-    var state = $("#lyricText").attr("state")
-    if (state === "uncensored") {
-      $("#lyricText").attr("state", "censored")
-      var censoredText = $("#lyricText").attr("dataCensored")
-      $("#lyricText").text(censoredText)
-    } else {
-      $("#lyricText").attr("state", "uncensored")
-      var uncensoredText = $("#lyricText").attr("dataUncensored")
-      $("#lyricText").text(uncensoredText)
-    }
-  })
-
-  //On click command for the submit button
 $("#submitButton").on("click", function(event) {
   event.preventDefault();
 
@@ -254,51 +106,43 @@ $("#submitButton").on("click", function(event) {
     $([document.documentElement, document.body]).animate(
       {
         scrollTop: $("#mainContent").offset().top
-      }, 2000);
+      },
+      2000
+    );
+  }
+
+  $(".image-cropper").css("background-image", "url()");
+  $("#artistBio").empty();
+  $("#artistName").empty();
+  $("#lyricText").empty();
+  $("#artistInput").val("");
+  $("#songTitleInput").val("");
+  
+
+  displayArtistInfo(songTitle, artist);
+
+  $("#profanityFilter").on("click", function() {
+    var state = $("#lyricText").attr("state");
+    if (state === "uncensored") {
+      $("#lyricText").attr("state", "censored");
+      var censoredText = $("#lyricText").attr("dataCensored");
+      $("#lyricText").text(censoredText);
+    } else {
+      $("#lyricText").attr("state", "uncensored");
+      var uncensoredText = $("#lyricText").attr("dataUncensored");
+      $("#lyricText").text(uncensoredText);
     }
-    
-
-    $(".image-cropper").css("background-image", "url()");
-    $("#artistBio").empty();
-    $("#artistName").empty();
-    $("#lyricText").empty();
-    $("#artistInput").val("");
-    $("#songTitleInput").val("");
-    artistNameArray = []
-    artistNameArray.push(artist);
-    songTitleArray = [];
-    songTitleArray.push(songTitle);
-
-    
-    displayArtistInfo(songTitle, artist);
-    
-    $("#profanityFilter").on("click", function (){
-      
-      var state = $("#lyricText").attr("state")
-      if (state === "uncensored"){
-        $("#lyricText").attr("state", "censored")
-        var censoredText = $("#lyricText").attr("dataCensored")
-        $("#lyricText").text(censoredText)
-      } 
-      else if (state === "censored") {
-        $("#lyricText").attr("state", "uncensored")
-        var censoredText = $("#lyricText").attr("dataUncensored")
-        $("#lyricText").text(uncensoredText)
-      }
-    })
+  });
 });
 
-})
-})
-
 //On click command to close the modals
-$(".closeButton").on("click", function (event) {
+$(".closeButton").on("click", function(event) {
   event.preventDefault();
 
   $(".bg-modal").css("display", "none");
 });
 
-$(".closeButton").on("click", function (event) {
+$(".closeButton").on("click", function(event) {
   event.preventDefault();
 
   $(".bg-modal2").css("display", "none");
@@ -309,11 +153,31 @@ var tl = new TimelineMax({ onUpdate: updatePercentage });
 var tl2 = new TimelineMax();
 const controller = new ScrollMagic.Controller();
 
-tl.from('#songMeter', 4, { x: -300, opacity: 0, ease: Power4.easeInOut }, "=-1");
-tl.from('#artistPhotoContainer', 2, { x: -500, opacity: 0, ease: Power4.easeInOut }, "=-1");
-tl.from('#videoContainer', 4, { x: -500, opacity: 0, ease: Power4.easeInOut }, "=-1");
-tl.from('#lyrics', 4, { x: -100, opacity: 0, ease: Power4.easeInOut }, "=-1");
-tl.from('#artistInfo', 4, { x: -500, opacity: 0, ease: Power4.easeInOut }, "=-1");
+tl.from(
+  "#songMeter",
+  4,
+  { x: -300, opacity: 0, ease: Power4.easeInOut },
+  "=-1"
+);
+tl.from(
+  "#artistPhotoContainer",
+  2,
+  { x: -500, opacity: 0, ease: Power4.easeInOut },
+  "=-1"
+);
+tl.from(
+  "#videoContainer",
+  4,
+  { x: -500, opacity: 0, ease: Power4.easeInOut },
+  "=-1"
+);
+tl.from("#lyrics", 4, { x: -100, opacity: 0, ease: Power4.easeInOut }, "=-1");
+tl.from(
+  "#artistInfo",
+  4,
+  { x: -500, opacity: 0, ease: Power4.easeInOut },
+  "=-1"
+);
 
 const scene = new ScrollMagic.Scene({
   triggerElement: ".sticky",
@@ -329,7 +193,6 @@ const scene2 = new ScrollMagic.Scene({
 })
   .setTween(tl2)
   .addTo(controller);
-
 
 function updatePercentage() {
   //percent.innerHTML = (tl.progress() *100 ).toFixed();
@@ -392,17 +255,16 @@ var meterFill = function(joy, suprise, fear, disgust, anger, sadness) {
 };
 
 //Hides opening animation on click of mouse
-$("#openingAnimationContainer").on("click", function () {
+$("#openingAnimationContainer").on("click", function() {
   $("#openingAnimationContainer").slideUp("slow");
 });
 
 //Fades in text "click to begin" within opening animation
-$(document).ready(function () {
+$(document).ready(function() {
   $("#openingClick").fadeIn(5000);
 });
 
 //Hides form content until user clicks on the page
-
 $(document).on("click", function() {
   $("#formContent").css("display", "flex");
-})
+});
